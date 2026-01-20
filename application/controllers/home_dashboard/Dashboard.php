92f7dc26b7f70470ec1e4dfcd2452d5cc7134c29
jonathan Junior <114232617+JonathanJr06@users.noreply.github.com>
2026-01-20 12:10:19 +0700

    Add login check in Dashboard constructor

--- application/controllers/home_dashboard/Dashboard.php
+++ application/controllers/home_dashboard/Dashboard.php
@@ -3,10 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller
 {
-
     public function __construct()
     {
         parent::__construct();
+        
+        // Cek apakah sudah login
         if (!$this->session->userdata('level')) {
             $this->session->set_flashdata('pesan', 'Anda harus masuk terlebih dahulu!');
             redirect('home');
@@ -16,12 +17,7 @@ class Dashboard extends CI_Controller
     public function index()
     {
         date_default_timezone_set('Asia/Jakarta');
-
-        $data['title']    = 'Dashboard';
-
-        // $data['bulanKemarin']        = date('F Y', strtotime('-1 month'));
-        // $data['belumbayarKemarin']    = $this->db->query('SELECT * FROM tb_master_data_barang WHERE id NOT IN (SELECT DISTINCT(idPelanggan) FROM tb_pembayaran WHERE MONTH(tanggal)="' . date('m', strtotime('-1 month')) . '" AND YEAR(tanggal)="' . date('Y') . '") AND status="Aktif"');
-        // $data['pelanggan']            = $this->db->query('SELECT * FROM tb_master_data_barang WHERE id NOT IN (SELECT DISTINCT(idPelanggan) FROM tb_pembayaran WHERE MONTH(tanggal)="' . date('m') . '" AND YEAR(tanggal)="' . date('Y') . '") AND status="Aktif"');
+        $data['title'] = 'Dashboard';
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar');
